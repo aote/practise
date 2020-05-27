6facbc763e62af8c4dba45952dc05d54ff34a4bb
wcf@blibao.cn <19821122>
2020-05-27 15:32:54 +0800

    commit

---
+++ src/main/java/com/aote/practise/controller/PaymentController.java
@@ -0,0 +1,14 @@
+package com.aote.practise.controller;
+
+/**
+ * @author: Weicf
+ * @date: 2020-05-27 15:28
+ * @description:
+ */
+public class PaymentController {
+
+    public void ex(){
+
+    }
+
+}

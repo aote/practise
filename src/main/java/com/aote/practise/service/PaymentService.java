91265ac2bab7edfc368aa5d0111356cb9a496404
wcf@blibao.cn <19821122>
2020-05-27 10:45:15 +0800

    first commit.

---
+++ src/main/java/com/aote/practise/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.aote.practise.service;
+
+/**
+ * @author: Weicf
+ * @date: 2020-05-27 10:43
+ * @description:
+ */
+public class PaymentService {
+}
---
+++ src/main/java/com/aote/practise/test/Test.java
@@ -0,0 +1,9 @@
+package com.aote.practise.test;
+
+/**
+ * @author: Weicf
+ * @date: 2020-05-27 10:36
+ * @description:
+ */
+public class Test {
+}
